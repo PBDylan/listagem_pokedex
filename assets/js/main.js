ff6dde350f7bc752b6820b3ec4e9c4381c293589
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-11-13 08:34:18 -0300

    Transformando lista de Pokémon em uma lista de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,32 @@
-// O fetch é um processamento assíncrono, onde acontece todo
-// aquele processamento de Cliente x Servidor. 
-// Promisse<Response> dentro de fetch(url) seria uma promessa de resposta ("Uma hora você vai receber sua resposta")
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToHTML(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#001</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+                <img src="https://w7.pngwing.com/pngs/984/841/png-transparent-bulbasaur-bulbasaur-pokemon-animation-thumbnail.png">
+            </div>
+        </li>
+    `;
+}
 
-const offset = 0
-const limit = 10
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+const pokemonList = document.getElementById('pokemonList');
 
 fetch(url)
-    .then{(response) => response.json()}
-    .then{(jsonBody) => console.log(jsonBody)}
-    .catch{(error) => console.log(error)}
-
-
-
-
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((results) => {
+        for (let i = 0; i < results.length; i++) {
+            const pokemon = results[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon);
+        }
+    })
+    .catch((error) => console.log(error));
