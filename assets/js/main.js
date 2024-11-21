8cdb01cce9fb3747c7d6817b3826d72c1d21776c
PBDylan <145608393+PBDylan@users.noreply.github.com>
2024-11-21 08:30:58 -0300

    Adicionando os tipos de Pokémon dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,26 @@
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => '<li class="type">${typeSlot.type.name}</li>')
-}
 
 function convertPokemonToHTML(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    ${convertPokemonTypesToLi(pokemon.types).join(' ')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
 
-                <img src="${pokemon.sprites.other.dream_world.front_default}">
+                <img src="${pokemon.photo}">
+                     alt="${pokemon.name}">   
             </div>
         </li>
-    `;
+    `
 }
 
-const pokemonList = document.getElementById('pokemonList');
+const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
-});
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
+})
 
